826cd22f3bc7b8f7ec2afd610461973ead15e811
matthew enbar <matthew.enbar@gmail.com>
2020-04-01 17:46:18 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,40 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true
+    else
+      return false
+    end
+
   end
 
   def question?
+    if self.end_with?("?")
+      return true
+    else
+      return false
+    end
 
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
-
+    sentance_array = self.split(/[\.\?\!]/)
+    i = 0
+    while i < sentance_array.size
+      if sentance_array[i] == ""
+        sentance_array.delete_at(i)
+      end
+      i += 1
+    end
+    sentance_array.size
   end
 end
